4c22a92f0b028d6847a3a3fc725659aa0f77e7fb
Felipe Jun Kumada <61765247+FelipeJun@users.noreply.github.com>
2022-03-02 22:14:30 -0300

    Escolha de cor, melhorado a IA com probabilidade

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,79 @@
 from cgi import print_form
 import chess
-import random
 import os
+
+pawntablew = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    5, 10, 10, -20, -20, 10, 10, 5,
+    5, -5, -10, 0, 0, -10, -5, 5,
+    0, 0, 0, 20, 20, 0, 0, 0,
+    5, 5, 10, 25, 25, 10, 5, 5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+pawntableb = pawntablew[::-1]
+
+knightstablew = [
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20, 0, 5, 5, 0, -20, -40,
+    -30, 5, 10, 15, 15, 10, 5, -30,
+    -30, 0, 15, 20, 20, 15, 0, -30,
+    -30, 5, 15, 20, 20, 15, 5, -30,
+    -30, 0, 10, 15, 15, 10, 0, -30,
+    -40, -20, 0, 0, 0, 0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50]
+
+knightstableb = knightstablew[::-1]
+
+bishopstablew = [
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10, 5, 0, 0, 0, 0, 5, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 0, 10, 10, 10, 10, 0, -10,
+    -10, 5, 5, 10, 10, 5, 5, -10,
+    -10, 0, 5, 10, 10, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20]
+
+bishopstableb = bishopstablew[::-1]
+
+rookstablew = [
+    0, 0, 0, 5, 5, 0, 0, 0,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    5, 10, 10, 10, 10, 10, 10, 5,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+rookstableb = rookstablew[::-1]
+
+queenstablew = [
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 5, 5, 5, 5, 5, 0, -10,
+    0, 0, 5, 5, 5, 5, 0, -5,
+    -5, 0, 5, 5, 5, 5, 0, -5,
+    -10, 0, 5, 5, 5, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20]
+
+queenstableb = queenstablew[::-1]
+
+kingstablew = [
+    20, 30, 10, 0, 0, 10, 30, 20,
+    20, 20, 0, 0, 0, 0, 20, 20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30]
+
+kingstableb = kingstablew[::-1]
+
 
 def clearConsole():
     command = 'clear'
@@ -9,11 +81,8 @@ def clearConsole():
         command = 'cls'
     os.system(command)
 
-# def randomChoice(b,moves):
-#     move = random.choice(list(moves))
-#     b.push(move)
 
-def minimaxRoot(depth, board,isMaximizing):
+def minimaxRoot(depth,board,isMaximizing):
     possibleMoves = board.legal_moves
     bestMove = -9999
     bestMoveFinal = None
@@ -22,7 +91,7 @@ def minimaxRoot(depth, board,isMaximizing):
         board.push(move)
         value = max(bestMove, minimax(depth - 1, board,-10000,10000, not isMaximizing))
         board.pop()
-        if( value > bestMove):
+        if value > bestMove:
             print("Best score: " ,str(bestMove))
             print("Best move: ",str(bestMoveFinal))
             bestMove = value
@@ -52,29 +121,9 @@ def minimax(depth, board, alpha, beta, is_maximizing):
             bestMove = min(bestMove, minimax(depth - 1, board,alpha,beta, not is_maximizing))
             board.pop()
             beta = min(beta,bestMove)
-            if(beta <= alpha):
+            if beta <= alpha:
                 return bestMove
         return bestMove
-
-
-def calculateMove(board):
-    possible_moves = board.legal_moves
-    if(len(possible_moves) == 0):
-        print("No more possible moves...Game Over")
-        sys.exit()
-    bestMove = None
-    bestValue = -9999
-    n = 0
-    for x in possible_moves:
-        move = chess.Move.from_uci(str(x))
-        board.push(move)
-        boardValue = -evaluation(board)
-        board.pop()
-        if(boardValue > bestValue):
-            bestValue = boardValue
-            bestMove = move
-
-    return bestMove
 
 def evaluation(board):
     i = 0
@@ -86,25 +135,24 @@ def evaluation(board):
         x = x
     while i < 63:
         i += 1
-        evaluation = evaluation + (getPieceValue(str(board.piece_at(i))) if x else -getPieceValue(str(board.piece_at(i))))
+        evaluation = evaluation + (getPieceValue(str(board.piece_at(i)),i) if x else -getPieceValue(str(board.piece_at(i)),i))
     return evaluation
 
 
-def getPieceValue(piece):
+def getPieceValue(piece,i):
     if(piece == None):
         return 0
     value = 0
     if piece == "P" or piece == "p":
-        value = 10
+        value = 10 + ((pawntablew[i]) if piece == "P" else (pawntableb[i]))
     if piece == "N" or piece == "n":
-        value = 30
+        value = 30 + ((knightstablew[i]) if piece == "N" else (knightstablew[i]))
     if piece == "B" or piece == "b":
-        value = 30
+        value = 30 + ((bishopstablew[i]) if piece == "B" else (bishopstableb[i]))
     if piece == "R" or piece == "r":
-        value = 50
+        value = 50 + ((rookstablew[i]) if piece == "R" else (rookstableb[i]))
     if piece == "Q" or piece == "q":
-        value = 90
+        value = 90 + ((queenstablew[i]) if piece == "Q" else (queenstableb[i]))
     if piece == 'K' or piece == 'k':
-        value = 900
-    #value = value if (board.piece_at(place)).color else -value
+        value = 900 + ((kingstablew[i]) if piece == "K" else (kingstableb[i]))
     return value
--- main.py
+++ main.py
@@ -1,39 +1,20 @@
 from funcoes import *
 
-# board = chess.Board()
-# legalMoves = board.legal_moves
-
-# #MAIN 
-# print("Bem Vindo ao Xadrez!")
-# print(board)
-
-# while not board.is_checkmate():
-#     if board.turn:
-#         print(list(legalMoves))
-#         move = chess.Move.from_uci(input("Faça sua jogada: "))
-#         if move in board.legal_moves:
-#             board.push(move)
-#         else:
-#             print("Jogada não valida, tente outra")
-#     else:
-#         # randomChoice(board,legalMoves)
-        
-        
-#     #clearConsole()
-#     print(board)
-
 board = chess.Board()
+playerColor = bool(int(input("Escolha a cor: Branco(1) ou preto(0)? ")))
+print(playerColor)
+
 print(board)
-while not board.is_checkmate():
-    if board.turn:
+while not board.is_checkmate() or not board.is_stalemate() or not board.is_insufficient_material():
+    if board.turn == playerColor:
         print(list(board.legal_moves))
-        move = input("Coloque sua jogada: ")
-        move = chess.Move.from_uci(str(move))
+        move = chess.Move.from_uci(input("Coloque sua jogada: "))
         board.push(move)
     else:
         print("Jogada da IA:")
         move = minimaxRoot(4,board,True)
         move = chess.Move.from_uci(str(move))
         board.push(move)
-    #clearConsole()
+    clearConsole()
     print(board)
+print("CABO O JOGO")
